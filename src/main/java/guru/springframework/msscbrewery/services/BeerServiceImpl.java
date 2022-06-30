ab1d6911a6c9724907e45a0ebd680b3508f85dc6
Mauro Castillo RondinaraÃ <mauro.castillo@globant.com>
2022-06-30 20:41:27 -0300

    add Delete beerById

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -2,8 +2,10 @@ package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.BeerDto;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
 
@@ -26,5 +28,10 @@ public class BeerServiceImpl implements BeerService {
   @Override
   public void updateBeer(UUID beerId, BeerDto beerDto) {
     //todo impl - would add a real impl to update beer
+  }
+
+  @Override
+  public void deleteBeer(UUID beerId) {
+    log.debug("Deleting beer ....");
   }
 }
--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -8,12 +8,14 @@ import java.util.UUID;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/beer")
@@ -43,10 +45,17 @@ public class BeerController {
     return new ResponseEntity(headers, CREATED);
   }
 
-  @PutMapping("/{beerId}")
+  @PutMapping({"/{beerId}"})
   public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
 
     beerService.updateBeer(beerId, beerDto);
     return new ResponseEntity(NO_CONTENT);
   }
+
+  @DeleteMapping({"/{beerId}"})
+  @ResponseStatus(NO_CONTENT)
+  public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+    beerService.deleteBeer(beerId);
+  }
+
 }
