5e5896546df40380babd09a65d7a4c811e72d3aa
Mauro Castillo RondinaraÃ <mauro.castillo@globant.com>
2022-06-30 19:54:32 -0300

    add create new handlePost

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -3,10 +3,12 @@ package guru.springframework.msscbrewery.web.controller;
 import guru.springframework.msscbrewery.web.model.BeerDto;
 import guru.springframework.msscbrewery.services.BeerService;
 import java.util.UUID;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +28,14 @@ public class BeerController {
     return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
   }
 
+  @PostMapping //POST - Create a new beer
+  public ResponseEntity handlePost(BeerDto beerDto) {
+
+    BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+    HttpHeaders headers = new HttpHeaders();
+    //todo add hostname to url
+    headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+    return new ResponseEntity(headers, HttpStatus.CREATED);
+  }
 }
